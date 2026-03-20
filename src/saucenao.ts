2fe23c694076503265ac3631dfbede8fae8c78c1
Kane Blueriver <kxxoling@gmail.com>
2026-03-20 16:34:50 +0800

    chore: upgrade dependencies and migrate to ES Modules

--- src/saucenao.ts
+++ src/saucenao.ts
@@ -1,18 +1,11 @@
-import sagiri from 'sagiri'
-
-declare global {
-  const SAUCENAO_TOKEN: string
-}
-
 export const dbmask = 34359738367
-
-export const client = sagiri(SAUCENAO_TOKEN)
 
 export async function requestImageSauce(
   imageURL: string,
-  minSimilarity: number = 70
+  minSimilarity: number = 70,
+  token: string
 ) {
-  const url = `http://saucenao.com/search.php?output_type=2&numres=10&dbmask=${dbmask}&api_key=${SAUCENAO_TOKEN}&url=${imageURL}`
+  const url = `http://saucenao.com/search.php?output_type=2&numres=10&dbmask=${dbmask}&api_key=${token}&url=${imageURL}`
 
   const resp = await fetch(url)
   const sauce: SauceNao.Response = await resp.json()
